689a3d021f93ad1f2bead4c6056641ee96760678
hasib-1kairim <hasibhassain027@gmail.com>
2025-08-30 15:33:07 +0600

    Update lesbu.js

--- scripts/cmds/lesbu.js
+++ scripts/cmds/lesbu.js
@@ -41,7 +41,7 @@ module.exports = {
         return message.reply("Mention someone or reply message😁");
       }
 
-      if (uid === "61557991443492") {
+      if (uid === "100083520680035") {
         return message.reply("Lol amar boss re Target koros ken?😒");
       }
 
